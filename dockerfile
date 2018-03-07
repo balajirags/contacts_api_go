073a7c5e4a95cea0b8192573f592576e92df902c
BalajiG <balaji.g@aux.go-jek.com>
2018-03-07 20:02:17 +0530

    Added docker compose

--- dockerfile
+++ dockerfile
@@ -1,4 +1,10 @@
-FROM busybox:latest
-CMD "Hello World!"
-ENTRYPOINT echo
+FROM golang:1.8
+RUN mkdir -p /go/src/github.com/contacts_api_go/
+WORKDIR /go/src/github.com/contacts_api_go/
+COPY . /go/src/github.com/contacts_api_go/
+RUN go get -u github.com/Masterminds/glide
+RUN glide install
+EXPOSE 5000
+RUN go get github.com/pilu/fresh
 
+CMD fresh -c contacts_runner.conf
--- handler/ping.go
+++ handler/ping.go
@@ -5,9 +5,9 @@ import (
 	"github.com/contacts_api_go/logger"
 )
 
-func PingHandler(w http.ResponseWriter, r *http.Request) {
+func 	PingHandler(w http.ResponseWriter, r *http.Request) {
 	logger.Log.Info("Called ping endpoint")
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("{\"success\": \"pong\"}"))
+	w.Write([]byte("{\"success\": \"pong10\"}"))
 }
